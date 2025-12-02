e40074babb1daa42d6e4623be62585cb8c43c102
jxt0807 <2483449682@qq.com>
2025-12-02 09:55:40 +0800

    xihe

--- examples/earth/xihe/work_dcu.sh
+++ examples/earth/xihe/work_dcu.sh
@@ -20,7 +20,7 @@ export HIP_VISIBLE_DEVICES=7
 python train_xihe2f.py
 
 ##### 8 DCU Train(change nproc_per_node to set DCU numbers) #####
-# torchrun --nproc_per_node=4 --nnodes=1 --rdzv_id=1000 --rdzv_backend=c10d --max_restarts=0 --master_addr="localhost" --master_port=29500 train.py
+# torchrun --nproc_per_node=4 --nnodes=1 --rdzv_id=1000 --rdzv_backend=c10d --max_restarts=0 --master_addr="localhost" --master_port=29500 train_xihe2f.py
 
 ##### 1 DCU Inference #####
 # python inference.py
