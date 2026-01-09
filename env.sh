67bda17ba3735a7ec722523ec4a0dd9293184083
jxt0807 <2483449682@qq.com>
2026-01-09 18:06:46 +0800

    add env.sh

--- env.sh
+++ env.sh
@@ -3,12 +3,12 @@ export ONESCIENCE_MODELS_DIR="/public/onestore/onemodels"
 
 # check datasets path
 if [ ! -d "$ONESCIENCE_DATASETS_DIR" ]; then
-  echo "❌️❌️❌️ ERROR: ONESCIENCE_DATASETS_DIR does not exist:"
+  echo "❌️❌️ ERROR: ONESCIENCE_DATASETS_DIR does not exist:"
   echo "    $ONESCIENCE_DATASETS_DIR"
 fi
 
 # check models path
 if [ ! -d "$ONESCIENCE_MODELS_DIR" ]; then
-  echo "❌❌️❌️ ERROR: ONESCIENCE_MODELS_DIR does not exist:"
+  echo "❌❌️ ERROR: ONESCIENCE_MODELS_DIR does not exist:"
   echo "    $ONESCIENCE_MODELS_DIR"
 fi
