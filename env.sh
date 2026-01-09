abe7cc465d275cb24569e119031441096026b343
jxt0807 <2483449682@qq.com>
2026-01-09 18:03:17 +0800

    undata env.sh

---
+++ env.sh
@@ -0,0 +1,14 @@
+export ONESCIENCE_DATASETS_DIR="/public/onestore/onedatasets"
+export ONESCIENCE_MODELS_DIR="/public/onestore/onemodels"
+
+# check datasets path
+if [ ! -d "$ONESCIENCE_DATASETS_DIR" ]; then
+  echo "❌️❌️❌️ ERROR: ONESCIENCE_DATASETS_DIR does not exist:"
+  echo "    $ONESCIENCE_DATASETS_DIR"
+fi
+
+# check models path
+if [ ! -d "$ONESCIENCE_MODELS_DIR" ]; then
+  echo "❌❌️❌️ ERROR: ONESCIENCE_MODELS_DIR does not exist:"
+  echo "    $ONESCIENCE_MODELS_DIR"
+fi
