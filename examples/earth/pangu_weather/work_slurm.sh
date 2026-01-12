d44a2c1c79dcda813693e6dea263b0afc3bfd0d3
zhaozhn_sugon <zhaozhn@sugon.com>
2026-01-12 02:05:15 +0000

    update examples/earth/pangu_weather/work_slurm.sh.
    Signed-off-by: zhaozhn_sugon <zhaozhn@sugon.com>

--- examples/earth/pangu_weather/work_slurm.sh
+++ examples/earth/pangu_weather/work_slurm.sh
@@ -15,13 +15,20 @@ module purge
 module load sghpcdas/25.6 
 conda init bash
 source ~/.bashrc
+
 ##### Activate Conda env #####
-conda activate era5newdata
+conda activate earth
+
 ##### Launch DTK #####
 module load sghpc-mpi-gcc/25.8
+
+##### Launch env #####
+source ../../../env.sh
+
 ##### Show env #####
 which python
 which hipcc
+
 ##### Set DCU #####
 export HIP_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
