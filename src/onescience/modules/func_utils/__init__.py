b21b704c90a511eba58ac7a92bbb58b9f41f1b26
wangxian <wangxian@sugon>
2026-01-26 12:03:40 +0800

    mokuaihua0126_0

--- src/onescience/modules/func_utils/__init__.py
+++ src/onescience/modules/func_utils/__init__.py
@@ -1,19 +1,13 @@
-from .patch_embed import (
-    PatchEmbed2D,
-    PatchEmbed3D,
-    PatchRecovery2D,
-    PatchRecovery3D,
-)
-from .shift_window_mask import (
-    get_shift_window_mask,
-    window_partition,
-    window_reverse,
-)
-from .utils import (
+from .pangu_utils import (
+    drop_path,
     crop2d,
     crop3d,
     get_earth_position_index,
     get_pad2d,
     get_pad3d,
+    get_shift_window_mask,
+    window_partition,
+    window_reverse,
+    trunc_normal_,
+    save_checkpoint
 )
-from .weight_init import trunc_normal_
