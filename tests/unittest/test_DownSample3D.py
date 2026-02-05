7502e2c18e099525ea64d5ea9fabe1b443fa489b
zhaozhn_sugon <zhaozhn@sugon.com>
2026-02-05 12:22:55 +0800

    add unit test

--- tests/unittest/test_DownSample3D.py
+++ tests/unittest/test_DownSample3D.py
@@ -4,5 +4,5 @@ from onescience.modules.resample.DownSample import DownSample3D
 downsample = DownSample3D(128, (13, 128, 256), (13, 64, 128))
 x = torch.randn(4, 425984, 128)
 out = downsample(x)
-print(out.shape)
+print('output:  ', out.shape)
 print(f'expected: torch.Size([4, 106496, 256])')
