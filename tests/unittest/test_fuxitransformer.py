7e456cb498586a0936beb1bddf8bab0ec9ebfb19
zhangjing8 <16629322+zhangjing8@user.noreply.gitee.com>
2026-03-03 17:05:25 +0800

    feat: add new unit test

--- tests/unittest/test_fuxitransformer.py
+++ tests/unittest/test_fuxitransformer.py
@@ -7,15 +7,15 @@ warnings.filterwarnings("ignore", message=".*torch.meshgrid.*")
 
 FuxiTransformer = OneTransformer(
     style="FuxiTransformer",
-    embed_dim=1536,
+    embed_dim=256,
     num_groups=32,
     input_resolution=(90, 180),
     num_heads=8,
     window_size=7,
     depth=48
 )
-x = torch.randn(2, 1536, 180, 360)  # (B*num_lon, num_lat, N, C)
+x = torch.randn(2, 256, 180, 360)  # (B*num_lon, num_lat, N, C)
 out = FuxiTransformer(x)
 print('Function: FuxiTransformer Forward Pass')
 print(f'output shape: {out.shape}')
-print( 'target shape: torch.Size([2, 1536, 180, 360])\n')
+print( 'target shape: torch.Size([2, 256, 180, 360])\n')
