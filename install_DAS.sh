b35f7caebbe5a5f4e3d6021c4188b6716c19be4e
zhaozhn_sugon <zhaozhn@sugon.com>
2026-03-25 16:02:07 +0800

    feat: add DAS install

---
+++ install_DAS.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 安装 whl 包
+pip install https://download.sourcefind.cn:65024/directlink/4/pytorch/DAS1.7/torch-2.5.1+das.opt1.dtk25042-cp311-cp311-manylinux_2_28_x86_64.whl
+pip install https://download.sourcefind.cn:65024/directlink/4/vision/DAS1.7/torchvision-0.20.1+das.opt1.dtk25042-cp311-cp311-manylinux_2_28_x86_64.whl
+pip install https://download.sourcefind.cn:65024/file/4/apex/DAS1.6/apex-1.4.0+das.opt1.dtk25041-cp311-cp311-manylinux_2_28_x86_64.whl
+pip install https://download.sourcefind.cn:65024/directlink/4/onnxruntime/DAS1.7/onnxruntime-1.19.2+das.opt1.dtk25042-cp311-cp311-manylinux_2_28_x86_64.whl
+pip install https://download.sourcefind.cn:65024/directlink/4/jax/DAS1.7/jax-0.4.34+das.opt1.dtk25042-py3-none-any.whl
+pip install https://download.sourcefind.cn:65024/directlink/4/jax/DAS1.7/jax_rocm60_pjrt-0.4.34+das.opt1.dtk25042-py3-none-manylinux2014_x86_64.whl
+pip install https://download.sourcefind.cn:65024/directlink/4/jax/DAS1.7/jax_rocm60_plugin-0.4.34+das.opt1.dtk25042-cp311-cp311-manylinux2014_x86_64.whl
+pip install https://download.sourcefind.cn:65024/directlink/4/jax/DAS1.7/jaxlib-0.4.34+das.opt1.dtk25042-cp311-cp311-manylinux2014_x86_64.whl
+pip install https://download.sourcefind.cn:65024/directlink/4/jax_triton/DAS1.7/jax_triton-0.2.0+das.opt1.dtk25042-py3-none-any.whl
+pip install https://download.sourcefind.cn:65024/directlink/4/triton/DAS1.7/triton-3.1.0+das.opt1.dtk25042-cp311-cp311-manylinux_2_28_x86_64.whl
+pip install https://download.sourcefind.cn:65024/directlink/4/transformer_engine/DAS1.7/transformer_engine-2.7.0+das.opt1.dtk25042-cp311-cp311-manylinux_2_28_x86_64.whl
+pip install https://download.sourcefind.cn:65024/directlink/4/flash_attn/DAS1.7/flash_attn-2.6.1+das.opt1.dtk25042-cp311-cp311-manylinux_2_28_x86_64.whl
+pip install https://download.sourcefind.cn:65024/file/4/fastpt/DAS1.7/fastpt-2.1.1+das.dtk25042-cp311-cp311-linux_x86_64.whl
+pip install https://download.sourcefind.cn:65024/directlink/4/dgl/DAS1.7/dgl-2.2.1+das.opt1.dtk25042-cp311-cp311-manylinux_2_28_x86_64.whl
+pip install https://download.sourcefind.cn:65024/directlink/9/onesicence/dtk-25.04.2/bionemo_noodles-0.1.2-cp311-cp311-manylinux_2_28_x86_64.whl
+pip install https://download.sourcefind.cn:65024/directlink/9/onesicence/dtk-25.04.2/nemo_toolkit-2.5.0rc0-py3-none-any.whl
+pip install https://download.sourcefind.cn:65024/file/4/tensorflow/DAS1.7/tensorflow-2.18.0+das.opt1.dtk25042-cp311-cp311-manylinux_2_28_x86_64.whl
