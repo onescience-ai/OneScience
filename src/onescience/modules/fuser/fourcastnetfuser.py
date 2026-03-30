4d7abff3dacb6c4f2a3a8bc385f5bf4637903f90
zhaozhn_sugon <zhaozhn@sugon.com>
2026-03-30 14:31:38 +0800

    fix: aadd package import for modules.fuser.fourcastnetfuser

--- src/onescience/modules/fuser/fourcastnetfuser.py
+++ src/onescience/modules/fuser/fourcastnetfuser.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from timm.models.layers import DropPath
 from onescience.modules.fc.onefc import OneFC
 from onescience.modules.afno.oneafno import OneAFNO
 
@@ -64,7 +65,6 @@ class FourCastNetFuser(nn.Module):
         self.norm1 = norm_layer(dim)
         self.filter = OneAFNO(style="FourCastNetAFNO2D")
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        #self.drop_path = nn.Identity()
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = OneFC(style="FourCastNetFC")
